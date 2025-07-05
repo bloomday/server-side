a0dafc17e5f80759285586674e6d66c609e57d5e
AdekunleM <adekunlemichael1319@gmail.com>
2025-07-05 11:10:16 +0100

    fixed the chrome ver. 138 error for puppeter

--- utils/generatePDF.js
+++ utils/generatePDF.js
@@ -1,7 +1,11 @@
 const puppeteer = require('puppeteer');
 
 const generatePDFBuffer = async (htmlContent) => {
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
 
@@ -15,3 +19,31 @@ const generatePDFBuffer = async (htmlContent) => {
 };
 
 module.exports = generatePDFBuffer;
+
+
+
+
+
+
+
+
+
+
+
+// const puppeteer = require('puppeteer');
+
+// const generatePDFBuffer = async (htmlContent) => {
+//   const browser = await puppeteer.launch({ headless: "new" });
+//   const page = await browser.newPage();
+//   await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
+
+//   const pdfBuffer = await page.pdf({
+//     format: 'A4',
+//     printBackground: true
+//   });
+
+//   await browser.close();
+//   return pdfBuffer;
+// };
+
+// module.exports = generatePDFBuffer;
